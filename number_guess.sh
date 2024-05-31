805b7cece36aabd834d7dded72286cf4ee0e0957
bruce zhang <shibozha@andrew.cmu.edu>
2024-05-31 17:26:07 +0000

    feat: hello_player

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+echo -e "\n~~~~ Guessing Number Game~~~~\n"
+echo -e "\nEnter your username:"
+read USERNAME
+SEARCH_RESULT=$($PSQL "SELECT * FROM records WHERE name='$USERNAME'")
+if [[ -z $SEARCH_RESULT ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  echo $SEARCH_RESULT | while IFS='|' read PLAYER_ID NAME GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
