33a6bd2b190ac3b6e8d51f4df762a0d4051798f9
bruce zhang <shibozha@andrew.cmu.edu>
2024-05-31 17:46:44 +0000

    feat: generate_random

--- number_guess.sh
+++ number_guess.sh
@@ -3,13 +3,15 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo -e "\n~~~~ Guessing Number Game~~~~\n"
 echo -e "\nEnter your username:"
 read USERNAME
-SEARCH_RESULT=$($PSQL "SELECT * FROM records WHERE name='$USERNAME'")
+SEARCH_RESULT=$($PSQL "SELECT * FROM records WHERE username='$USERNAME'")
 if [[ -z $SEARCH_RESULT ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_NEW_PLAYER=$($PSQL "INSERT INTO records(username) VALUES('$USERNAME')")
 else
-  echo $SEARCH_RESULT | while IFS='|' read PLAYER_ID NAME GAMES_PLAYED BEST_GAME
-  do
-    echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-  done
+  IFS='|' read PLAYER_ID USERNAME GAMES_PLAYED BEST_GAME <<< "$SEARCH_RESULT"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+# 生成随机数
+RANDOM_NUMBER=$(( ( RANDOM % 1000 ) + 1 ))
